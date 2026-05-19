227641815a2af5eff61c392d15a3282c0a968e37
Colby Saigeon <csaig929@proton.me>
2026-05-18 22:13:41 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ module = Extension(
 
 setup(
     name='mftparser',
-    version='1.0.0',
+    version='1.6.0',
     packages=['mftparser'],
     options={'bdist_wheel': {'py_limited_api': 'cp39'}},
     ext_modules=[module],
